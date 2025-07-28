809047c8d2fd7eb12dda998406d13efd52d35884
MisaelSalazar <misaelsalazarof@protonmail.com>
2025-07-28 12:57:47 -0600

    Proyecto Iniciado

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+function App() {
+
+  return (
+    <div
+      style={{
+        margin: '2rem'
+      }}
+    >
+      <h1 className="d-flex justify-content-center">Sistema Gestor de Finanzas Personales</h1>
+
+      <nav>
+        <button className="btn btn-success">
+          Ingresos
+        </button>
+        <button className="btn btn-danger">
+          Gastos
+        </button>
+      </nav>
+    </div>
+  )
+}
+
+export default App
